633ef5e0a0aef6cfdfc61f5ae313b01626757771
Miroslav Vrbovsky <miroslav.vrbovsky@amadeus.com>
2021-11-15 15:05:02 +0100

    Add script which creates an area in Azure DevOps

---
+++ azdevops/create-areas/create-areas.ps1
@@ -0,0 +1,42 @@
+# Read encrypted token from a text file
+$EncryptedPersonalAccessToken = Get-Content("credentials.cfg")
+
+# Convert encrypted token to SecureString
+$SecurePersonalAccessToken = $EncryptedPersonalAccessToken | ConvertTo-SecureString
+
+# Read Azure DevOps parameters from a text file
+Get-Content -Path .\params.txt -ReadCount 2 | ForEach-Object {
+    $SplattedParameters = ConvertFrom-StringData $($_ -join [Environment]::NewLine)
+}
+
+# Unnecessary step just to save time
+$OrganizationName = $SplattedParameters.OrganizationName
+$ProjectName = $SplattedParameters.ProjectName
+
+# Convert SecureString to plaintext
+$BSTR = `
+    [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePersonalAccessToken)
+$PlainPersonalAccessToken = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+# Define authentication method
+$BasicAuth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f '', $PlainPersonalAccessToken)))
+
+# Create authorization header
+$AuthorizationHeader = @{
+    Authorization = "Basic $BasicAuth"
+}
+
+# Create request body
+$Area = @{
+    name = "Another area name2"
+    }
+
+# Convert request body to JSON
+$JsonBody = $Area | ConvertTo-Json
+
+# Submit POST request to Azure DevOps API to create the area
+$Area = Invoke-RestMethod -Method POST `
+    -Uri $OrganizationName/$ProjectName/_apis/wit/classificationnodes/Areas?api-version=6.0 `
+    -ContentType "application/json" `
+    -Headers $AuthorizationHeader `
+    -body $JsonBody
